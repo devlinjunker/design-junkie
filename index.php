5a2041946a780fbe7a2bf6fc05bf8b4efd369993
devlinjunker <devlin@osu>
2012-08-05 22:40:47 -0700

    replace h3->h4

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 <?php include 'header.htm'; ?>
 
 <div id="about">
-	<h3>About Me</h3>
+	<h4>About Me</h4>
 </div>
 <div id="projects">
-	<h3>Projects</h3>
+	<h4>Projects</h4>
 	<ul>
 		<li style='display:inline;'>#1</li>
 		<li style='display:inline;'>#2</li>
 	</ul>
 </div>
 <div id="links">
-	<h3>Links</h3>
+	<h4>Links</h4>
 </div>
 	
 <?php include 'footer.htm' ?>
