e2126ce2a93311e87875b955e1fd651da8d69ff3
devlinjunker <devlin@osu>
2012-08-06 18:40:08 -0700

    rounding corners and adding dropshadow

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php $pagetitle="Home"; ?>
 <?php include 'header.htm'; ?>
 
-<div id="about">
+<div id="about" class="section">
 	<h4>About Me</h4>
 </div>
-<div id="projects">
+<div id="projects" class="section">
 	<h4>Projects</h4>
 	<ul>
 		<li style='display:inline;'>#1</li>
 		<li style='display:inline;'>#2</li>
 	</ul>
 </div>
-<div id="links">
+<div id="links" class="section">
 	<h4>Links</h4>
 </div>
 	
