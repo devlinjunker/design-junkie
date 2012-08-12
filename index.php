71c6a59f96a2b738a6fa57dee6d32c74b3f3641f
devlinjunker <devlin@osu>
2012-08-11 18:47:40 -0700

    added contact buton

--- index.php
+++ index.php
@@ -12,15 +12,20 @@
 
 <div id="links" class="section">
 	<h4>Links</h4>
+	<ul id="linklist">
+		<li></li>
+	</ul>
 </div>
 
 <div id="about" class="section">
-	<h4>About Me</h4>
+	<h4>About Me <a href="<?php echo $sitepath;?>/about" class="btn">Contact Me</a></h4>
 	<img id="myphoto" src="<?php echo $sitepath;?>/img/myphoto.jpg"></img>
 	<div id="text">
-		My name is Devlin Junker, I'm an undergraduate student in Computer Science at Oregon State University. 
+		My name is Devlin Junker, I'm an undergraduate student in Computer Science at Oregon State 
+		University. 
 		<br/><br/>
-		I am interested in web and mobile application development and I have experience in MySQL and wiith relational
+		I am interested in web and mobile application development and I have experience 
+		using HTML, CSS, Javascript, AJAX, Ruby and PHP in conjuction with MySQL and relational
 		database's to track and store information for organizations. 
 	</div>
 	
