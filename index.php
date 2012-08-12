89b1d152a123e4817e759a7850918fbd5b5f17a0
devlinjunker <devlin@osu>
2012-08-11 17:30:50 -0700

    width -> percentages

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php $pagetitle="Home"; ?>
 <?php include 'header.htm'; ?>
 
-<div id="about" class="section">
-	<h4>About Me</h4>
+
+<div id="links" class="section">
+	<h4>Links</h4>
 </div>
-<div id="projects" class="section">
+<div id="content" class="section">
 	<h4>Projects</h4>
 	<ul>
 		<li style='display:inline;'>#1</li>
 		<li style='display:inline;'>#2</li>
 	</ul>
 </div>
-<div id="links" class="section">
-	<h4>Links</h4>
+<div id="about" class="section">
+	<h4>About Me</h4>
 </div>
+
 	
 <?php include 'footer.htm' ?>
