e663032a1c35116c9251e04357aa0ab0c273f55a
devlinjunker <devlin@osu>
2012-08-12 23:26:46 -0700

    fixed contact form module include

--- index.php
+++ index.php
@@ -3,15 +3,18 @@
 
 <div id="projects" class="section">
 	<h4>Projects</h4>
+	
 	<ul>
 		<li>#1</li>
 		<li>#2</li>
 	</ul>
+	
 	<h4>Labs</h4>
 </div>
 
 <div id="links" class="section">
 	<h4>Links</h4>
+	
 	<ul id="linklist">
 		<li><a href="">LinkedIn</a></li>
 		<li><a href="">Resume</a></li>
@@ -21,7 +24,9 @@
 
 <div id="about" class="section">
 	<h4>About Me</h4>
+	
 	<img id="myphoto" src="<?php echo $sitepath;?>/img/myphoto.jpg"></img>
+	
 	<div id="text">
 		My name is Devlin Junker, I'm an undergraduate student studying Computer Science at Oregon 
 		State University with a focus on usability engineering. The computer network and service industry
@@ -33,11 +38,13 @@
 		Ruby and PHP in conjuction with AJAX, MySQL and relational database's to track, manage and 
 		store information for groups and organizations. 
 	</div>
+	
 	<a href="javascript:" class="btn">Quick Contact</a>
+	
 	<form id="quickcontact" class="contactform">
 		<?php 
 			$scripts[] = "contactform.js";
-			require(contactform.php);
+			include(contactform.php);
 		?>
 	</form>
 </div>
